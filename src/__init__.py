7c27932dea0a246f1ad9cf87b96320d2105b9bf3
John James <john.james.ai.studio@gmail.com>
2022-07-21 04:15:07 -0400

    regression

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Deep Learning with Tensorflow                                                       #
+# Version    : 0.1.0                                                                               #
+# Filename   : /__init__.py                                                                        #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/tensorflow-deep-learning                           #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Thursday July 21st 2022 03:42:55 am                                                 #
+# Modified   : Thursday July 21st 2022 03:42:59 am                                                 #
+# ------------------------------------------------------------------------------------------------ #
+# License    : BSD 3-clause "New" or "Revised" License                                             #
+# Copyright  : (c) 2022 John James                                                                 #
+# ================================================================================================ #
